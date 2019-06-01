ac18393ad498a7b67270cd200bd1fde3c7c202be
Lauren Carne <oreocookie@hotmail.co.uk>
2019-06-01 10:37:10 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const names = ["Ada", "Brendan", "Ali"];
+
+function writeCards(array, string) {
+  let thankYouCards = []
+  for (let i = 0; i < array.length; i++)
+  {
+    thankYouCards.push(`Thank you, ${array[i]}, for the wonderful ${string} gift!`)
+  }
+  return thankYouCards
+}
+
+function countdown(number) {
+  while (number > 0) {
+    console.log(number);
+    number -= 1;
+  }
+  return console.log(number)
+}
