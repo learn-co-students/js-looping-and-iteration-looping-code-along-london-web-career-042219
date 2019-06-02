cb7cc85f17545192c640315dfaed891f99e937a3
miaoshan <minnieshan@gmail.com>
2019-06-02 22:34:32 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function countdown(num){
+  while (num>0){
+  console.log (num);
+  num-=1;
+  }
+  return console.log(num)
+  }
+
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
