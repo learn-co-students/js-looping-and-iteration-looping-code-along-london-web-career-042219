ef86bc7351ce06d9ceaf58fe1ce190454f0f044c
dunia-and-the-code <dunia-al-baghdadi@hotmail.com>
2019-06-05 13:39:29 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+//Greetings cards for loop
+const names = ['Lisa', 'Kaitlin', 'Jan']
+
+function writeCards(names){
+    let cards = [];
+    for(let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+    }
+    return cards
+}
+
+//Countdown while loop
+function countdown(number){
+    let i = number;
+    while(i >= 0){
+        console.log(i)
+        --i
+    }
+}
